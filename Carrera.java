64f4d758760fbb20a70fb8e8436c83de80b37384
Manuel <manuel@masiasweb.com>
2022-03-06 13:00:44 +0100

    Generalizado numeroCaballos y longitudPista

--- Carrera.java
+++ Carrera.java
@@ -1,24 +1,27 @@
 class Carrera {
 
-    private int numeroPistas = 5;
+    private int numeroCaballos = 2;
+    private int longitudPista = 25;
     private Pista pista;
     private Caballo[] caballos;
     private Turno turno;
 
     public Carrera() {
-        pista = new Pista(20,numeroPistas);
-        caballos = new Caballo[numeroPistas];
-        for (int i=0;i<numeroPistas;i++) {
-            caballos[i] = new Caballo(i+1);
-        }        
-        turno = new Turno(numeroPistas);
+        pista = new Pista(longitudPista, numeroCaballos);
+        caballos = new Caballo[numeroCaballos];
+        for (int i = 0; i < numeroCaballos; i++) {
+            caballos[i] = new Caballo(i + 1);
+        }
+        turno = new Turno(numeroCaballos);
     }
 
     public void jugar() {
         do {
             pista.mostrar();
             caballos[turno.toca()].moverse(pista);
-            if (!pista.hayGanador()) {turno.cambiar();}
+            if (!pista.hayGanador()) {
+                turno.cambiar();
+            }
         } while (!pista.hayGanador());
         caballos[turno.toca()].celebraVictoria();
     }
